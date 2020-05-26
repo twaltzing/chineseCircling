be155d1bf946e7ca52a3dbab61ce295fda18c20c
twaltzing <terry.waltz@chineseandspanish.com>
2020-05-26 16:42:05 -0400

    add questions and exceptions

--- javascript/variedQuestionsJSON.js
+++ javascript/variedQuestionsJSON.js
@@ -104,6 +104,8 @@ mei="沒";
 or="還是";
 ma="嗎";
 
+type="";
+
 ////set up the data 
 let story = [];
 let sentence=[];
@@ -115,8 +117,8 @@ let answer="";
 let shortAnswer="";
 
 //add questions to the question/answer list for this sentence
-function addQA(question, answer, shortAnswer){
-    QA.push(question, answer, shortAnswer);
+function addQA(qtype, question, answer, shortAnswer){
+    QA.push(qtype, question, answer, shortAnswer);
     QAarray.push(QA);
     QA=[];
     return QAarray;
@@ -146,63 +148,130 @@ function randomOrder(){
     return orderNo;
 }
 
-
+function stripSpaces(string){
+    string  = string.replace(/\s{2,}/g, ' ');
+    string=string.trim();
+    return string;
+}
 
 // SET THE STATEMENT:
 statement=mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes;
+statement=stripSpaces(statement);
+statement=statement.trim();
+statement=statement+"。";
 
     // Yes question with ma
-question = statement + ma + "?";
+
+
+
+qtype="Yes-ma";
+question=statement.substring(0, statement.length - 1) + sp + ma + "？";
+
 answer= statement;
-shortAnswer=mMainVerb + mAspect;
-addQA(question, answer, shortAnswer);
+if(mAspect.length>0){
+    shortAnswer=mAspect;
+}  else {
+shortAnswer=mMainVerb;
+}
+
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
 
     // Yes question with V不V
+qtype="YES-VbuV";
+
     if (mMainVerb==="有") {
         neg="沒";
-    }
+    } 
+
+    if (mPlace !=="") {
+question = mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ "在 不"+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb  + sp +mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes;
+shortAnswer="在";
+    } else {
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ neg + sp + mMainVerb + sp +mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes;
+
 answer=statement;
 shortAnswer=mMainVerb + mAspect;
-addQA(question, answer, shortAnswer);
+    }
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
 
 
 
     ///No question: subject
+qtype="";
+ 
 if (mSub.length>0){
+    qtype="NO-subject";
         //don't do for subjectless S
 question = mSubAdj + sp + randomShadow(sSub) + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes + sp + ma+"?";
 answer=statement;
 shortAnswer=mMainVerb + mAspect;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
 }
 
 
 //// Who? subject question
+qtype="";
+ 
+if (mSub.length>0){
+qtype="QW-subject";
 question = "誰" + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes +"?";
 answer = statement;
 shortAnswer=mSubAdj + sp + mSub;
-addQA(question, answer, shortAnswer);
-
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 ///subject OR question
-question = mSubAdj + sp + randomShadow(sSub) + sp +or+mSub+sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes + sp + ma+"?";
+qtype="";
+ 
+if (mSub.length>0){
+
+qtype="OR-subject";
+question = mSubAdj + sp + randomShadow(sSub) + sp +or+mSub+sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes+"？";
 answer=statement;
 shortAnswer=mSub;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 ////No object question
+qtype="";
+ 
+if (mObj.length>0){
+
+qtype="NO-object";
 Shadow=randomShadow(sObj);
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ Shadow +sp+ mTimes+sp+ma +"?";
 answer=statement;
 // deny=mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ neg+sp+mModal +sp+ mGen +sp+ mMainVerb +sp+ mNum +sp+ mMeas +sp+ Shadow +sp+ mTimes;
 shortAnswer=mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 //// Object or question
+qtype="";
+ 
+if (mObj.length>0){
+qtype="OR-object";
 Shadow=randomShadow(sObj);
 
 if(randomOrder()===1){
@@ -211,28 +280,48 @@ if(randomOrder()===1){
     var adjs= mObjAdj + mObj + sp + or + mObjAdj + Shadow;
 }
 
-question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ Shadow +sp+ or+ mNum +sp +mMeas +sp+ +mObjAdj+sp+ mObj + sp+ mTimes+"?";
+question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ Shadow +sp+ or+ mNum +sp +mMeas +sp+ +mObjAdj+sp+ mObj + sp+ mTimes+"？";
 answer=statement;
 shortAnswer=mNum +sp +mMeas +sp+ mObjAdj + sp + mObj;
 
-addQA(question, answer, shortAnswer);
-
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 ///// Object WH question
+qtype="";
+ 
+if (mObj.length>0){
+qtype="WH-object";
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ "什麼"+sp+ mTimes+"?";
 answer=statement;
 shortAnswer=mNum+sp+mMeas+sp+mObjAdj+sp+mObj;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 //// What is Sub doing?
-    if (mModal=""){
+qtype="WH-Verb";
+
+///may need a case for Zai sentences, no main verb at all, as distinct from sentences with main verb and location
+    //if no modal and not a "you" sentence
+    if (mModal=""&& mMainVerb !=="有"){
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ "做 什麼?";
 answer=statement;
 shortAnswer=mVerb;
     }
 
+    console.log(mMainVerb !=="有" ||( mPlace.length>0 && mMainVerb.length>0));
 //// Verb or question
+qtype="";
+ 
+if (mMainVerb !=="有" && (mPlace.length>0 && mMainVerb.length>0)){
+qtype="OR-verb";
 Shadow=randomShadow(sMainVerb);
     
 if(randomOrder()===1){
@@ -241,22 +330,40 @@ if(randomOrder()===1){
     var verbs= Shadow + mAspect + sp + or + sp + mMainVerb + mAspect;
 }
 
-question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ verbs +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes+"?";
+question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ verbs +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes+"？";
 answer=statement;
 shortAnswer=mMainVerb+sp+mAspect;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
+
 
 ///Verb no question
+qtype="";
+ 
+if (mMainVerb !=="有"){
+
+qtype="NO-verb";
 Shadow=randomShadow(sMainVerb);
 
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ Shadow +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes+ma +"?";
 answer=statement;
 shortAnswer=mMainVerb +sp + mAspect;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 ///Object adj NO question
+qtype="";
+ 
+if (mObjAdj.length>0){
+qtype="NO-object adj";
 Shadow=randomShadow(sObjAdj);
 
 question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ Shadow+ sp + mObj +sp+ mTimes+ma +"?";
@@ -264,9 +371,17 @@ question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal 
 answer=statement;
 shortAnswer=mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 ///Object adj OR question
+qtype="";
+ 
+if (mObjAdj.length>0){
+qtype="OR-object adj";
 Shadow=randomShadow(sObjAdj);
 if(randomOrder()===1){
     var adjs= mObjAdj + mObj + sp + or + Shadow + mObj; // put main adj first
@@ -274,20 +389,94 @@ if(randomOrder()===1){
     var adjs= Shadow + mObj + sp + or + mObjAdj + mObj;
 }
 
-question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ adjs +sp+ mTimes+ma +"?";
+question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ adjs +sp+ mTimes+"？";
 
 answer=statement;
 shortAnswer=mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj;
 
-addQA(question, answer, shortAnswer);
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
+
+/////// Zai location question
+qtype="WH-where";
+if(mPlace !==""){
+question = mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ "在 哪兒" +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes + "？";
+
+answer = statement;
+shortAnswer = mPlace;
+
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
 
 
 
-///////
+
+
+
+// console.log(mGen[0]);
+// genStarter=(mGen[0]);
+
+
+///// With whom question
+
+qtype="";
+ 
+if (mGen.length>0){
+
+switch (mGen[0]){
+    case "跟":
+        insert="跟 誰";
+        break;
+    case "給":
+        insert="給 誰";
+        break;
+     case "為":
+         insert="為 誰";
+         break;
+}
+
+    qtype="WH-with whom";
+question = mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ insert +sp+ mMainVerb +sp+ mAspect +sp+ mNum +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes;
+// console.log("This is a 跟 sentence!");
+
+answer= statement;
+shortAnswer = mGen;
+
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
+//////// How-many objects?
+
+if (mNum !=="" && mMeas !==""){
+
+question =mSubAdj + sp + mSub + sp + mTime + sp + mDate +sp+ mPlace +sp+ mModal +sp+ mGen +sp+ mMainVerb +sp+ mAspect +sp+ "幾" +sp+ mMeas +sp+ mObjAdj +sp+ mObj +sp+ mTimes;
+answer=statement;
+shortAnswer=mNum + sp + mMeas;
+
+question=stripSpaces(question);
+answer=stripSpaces(answer);
+shortAnswer=stripSpaces(shortAnswer);
+addQA(qtype, question, answer, shortAnswer);
+}
+///////////
+
+
+
+/////// Make the story one sentence longer
 sentenceSet=makeSentenceSet(statement, QAarray);
 addSentenceToStory(sentenceSet);
 console.log(story);
 
+var JSONdata = JSON.stringify(story);
+console.log(JSONdata);
 
 
 function randomShadow(array){
